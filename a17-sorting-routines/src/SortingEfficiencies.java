3c353a35651311e9e9bdf9ec52b768dbf0fe863c
ChanAlvin <67116205+ChanAlvin@users.noreply.github.com>
2022-06-08 12:59:20 -0400

    drunk gui
    Made the baseline example GUI from assignment page
    Design is very crooked in some places; will fix soon

--- a17-sorting-routines/src/SortingEfficiencies.java
+++ a17-sorting-routines/src/SortingEfficiencies.java
@@ -4,6 +4,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JScrollBar;
 
 public class SortingEfficiencies extends JFrame {
 
@@ -30,11 +35,74 @@ public class SortingEfficiencies extends JFrame {
 	 */
 	public SortingEfficiencies() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 669, 401);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Sorting Efficiencies");
+		lblNewLabel.setBounds(257, 11, 109, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Enter the amount of numbers in the list:");
+		lblNewLabel_1.setBounds(10, 31, 198, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("10");
+		rdbtnNewRadioButton.setBounds(214, 32, 51, 23);
+		contentPane.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("100");
+		rdbtnNewRadioButton_1.setBounds(267, 32, 46, 23);
+		contentPane.add(rdbtnNewRadioButton_1);
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("1000");
+		rdbtnNewRadioButton_2.setBounds(318, 32, 57, 23);
+		contentPane.add(rdbtnNewRadioButton_2);
+		
+		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("5000");
+		rdbtnNewRadioButton_3.setBounds(377, 32, 57, 23);
+		contentPane.add(rdbtnNewRadioButton_3);
+		
+		JButton btnNewButton = new JButton("Sort Numbers");
+		btnNewButton.setBounds(20, 56, 125, 23);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel_2 = new JLabel("Sort Order");
+		lblNewLabel_2.setBounds(10, 102, 65, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JRadioButton rdbtnNewRadioButton_4 = new JRadioButton("Ascending");
+		rdbtnNewRadioButton_4.setBounds(74, 98, 109, 23);
+		contentPane.add(rdbtnNewRadioButton_4);
+		
+		JRadioButton rdbtnNewRadioButton_5 = new JRadioButton("Descending");
+		rdbtnNewRadioButton_5.setBounds(74, 124, 109, 23);
+		contentPane.add(rdbtnNewRadioButton_5);
+		
+		JLabel lblNewLabel_3 = new JLabel("Original Numbers");
+		lblNewLabel_3.setBounds(10, 157, 89, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Sorted Numbers");
+		lblNewLabel_4.setBounds(103, 154, 80, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 178, 65, 173);
+		contentPane.add(textArea);
+		
+		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setBounds(103, 178, 65, 173);
+		contentPane.add(textArea_1);
+		
+		JLabel lblNewLabel_5 = new JLabel("Sort Results");
+		lblNewLabel_5.setBounds(220, 80, 89, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		JTextArea textArea_2 = new JTextArea();
+		textArea_2.setBounds(213, 108, 430, 243);
+		contentPane.add(textArea_2);
 	}
-
 }
